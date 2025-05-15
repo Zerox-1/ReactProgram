d07af97da81e01fb5e03d7055c8118d0ad05af6e
Zerox-1 <1nprogh1@gmail.com>
2025-05-15 17:41:49 +0300

    feat: commit for checking

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+app.use(errorMiddleware)
 app.get('/',(req,res)=>{
     res.status(200).json({message:'Yeah! Its work!'})
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 import offerRouter from './offerRoutes.js';
-
+import userRotes from './userRoutes.js';
 
 
 const router=new Router();
 
 router.use('/',offerRouter);
+router.use('/',userRotes);
 
 export {router}
